c5698b14ca29d8e30527d9ed466ac122dc4e0a94
Sean <smcgovern135@gmail.com>
2021-08-14 11:45:41 -0400

    init routes

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
   res.send('hello world!')
 })
 
+app.get('/products', (req, res) => {})
+app.get('/products:product_id', (req, res) => {})
+app.get('/products:product_id/styles', (req, res) => {})
+app.get('/products:product_id/related', (req, res) => {})
+
 app.listen(port, () => {
   console.log(`server listening on port ${port}`)
   console.log(db.readyState)
