96fb923793d572e82a365ec9fb30661c6418390a
Sean <smcgovern135@gmail.com>
2021-08-17 16:39:22 -0400

    added index for product collection

--- src/db.ts
+++ src/db.ts
@@ -18,13 +18,14 @@ db.once('open', () => {
     collection: 'skus'
   })
 
-  //indexes used in styles query
+  //indexes used in API queries
   skuSchema.index({id: 1})
   styleSchema.index({productId: 1})
+  productSchema.index({id: 1})
 
-  const Product = mongoose.model('Product', productSchema);
-  const Style = mongoose.model('Style', styleSchema);
-  const Sku = mongoose.model('Sku', skuSchema);
+  const Product = mongoose.model('Product', productSchema)
+  const Style = mongoose.model('Style', styleSchema)
+  const Sku = mongoose.model('Sku', skuSchema)
 })
 
 export default db
